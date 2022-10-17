246858329a61c4f059012963ee2166cfbe8b9dc7
唐澤 克幸 <70247011+Katsuyuki-Karasawa@users.noreply.github.com>
2022-10-17 13:17:59 +0900

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const company = 
   {
     "name": "唐澤 克幸",
-    "zipCode": "136-0073",
+    "zipCode": "",
     "address": "4ranci0ne@gmail.com",
     "tel": "080-7851-5367",
     "logoPath": "./images/logo.svg",
